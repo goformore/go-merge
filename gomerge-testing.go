83d2c1f8780306381cb97658c95ea7065eb4948c
jigarnayak1290 <jigarnayak1290@gmail.com>
2024-12-07 13:52:09 +0900

    Gpr code used for testing

---
+++ gomerge-testing.go
@@ -0,0 +1,10 @@
+// main.go
+package main
+
+func main() {
+	// Create an instance of CustomPrinter
+	cp := CustomPrinter{}
+
+	// Use the custom Println method
+	cp.Println("Hello, World!")
+}
--- gomerge.go
+++ gomerge.go
@@ -1,13 +1,24 @@
-package gomerge
+package main
 
 import (
 	"fmt"
 )
 
-type Gomerge struct {
+// CustomPrinter struct that holds additional functionality
+type CustomPrinter struct{}
+
+// Println method that wraps fmt.Println
+func (cp CustomPrinter) Println(a ...interface{}) (n int, err error) {
+	// Call the original Println function
+	n, err = fmt.Println(a...)
+
+	// Perform additional actions after Println completes
+	cp.afterPrintln()
+
+	return n, err
 }
 
-func (g *Gomerge) Println(a ...any) (n int, err error) {
-	str := a
-	return fmt.Println(str)
+// afterPrintln performs additional actions after Println completes
+func (cp CustomPrinter) afterPrintln() {
+	fmt.Println("This message is printed after the original Println.")
 }
